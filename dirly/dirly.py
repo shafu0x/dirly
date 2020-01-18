9231f91713db277cecfc1ff4d20726ad6b717a6c
Sharif Elfouly <selfouly@gmail.com>
2020-01-18 18:49:05 +0100

    added get_files() and restructured API

--- dirly/dirly.py
+++ dirly/dirly.py
@@ -1,28 +1,80 @@
-import os
+import os, mimetypes
+from pathlib import Path
+
+img_extensions = set(k for k,v in mimetypes.types_map.items() if v.startswith('image/'))
+
+def _get_files(parent, p, f, extensions):
+    p = Path(p) 
+    if isinstance(extensions, str):
+        extensions = [extensions]
+    low_extensions = [e.lower()
+                      for e in extensions] if extensions is not None else None
+    res = [p/o for o in f if not o.startswith('.')
+           and (extensions is None or f'.{o.split(".")[-1].lower()}' in low_extensions)]
+    return res
+
+def get_files(path, extensions=None, recurse=False, exclude=None, include=None,
+                presort=False, followlinks=False):
+"Return list of files in `path` that have a suffix in `extensions`; optionally `recurse`."
+if recurse:
+    res = []
+    for i, (p, d, f) in enumerate(os.walk(path, followlinks=followlinks)):
+        # skip hidden dirs
+        if include is not None and i == 0:
+            d[:] = [o for o in d if o in include]
+        elif exclude is not None and i == 0:
+            d[:] = [o for o in d if o not in exclude]
+        else:
+            d[:] = [o for o in d if not o.startswith('.')]
+        res += _get_files(path, p, f, extensions)
+    if presort:
+        res = sorted(
+            res, key=lambda p: _path_to_same_str(p), reverse=False)
+    return res
+else:
+    f = [o.name for o in os.scandir(path) if o.is_file()]
+    res = _get_files(path, path, f, extensions)
+    if presort:
+        res = sorted(
+            res, key=lambda p: _path_to_same_str(p), reverse=False)
+    return res
 
 class dirly:
-    def __init__(self, i, o=None, ext=None):
-        self.i, self.o = i, o
+    def __init__(self, i, o=None, ext=None, recurse=False):
+        self.i = Path(i)
+        if o:
+            self.o = Path(o)
+            if not self.o.exists(): os.mkdir(o)
         self.ext = ext
-        if o and not os.path.exists(o):
-            os.mkdir(o)
+        self.recurse = recurse
+
     def __call__(self, *args):
         def fn(*kwargs):
             o_s = []
-            for fp in os.listdir(self.i):
-                o = args[0](os.path.join(self.i, fp), kwargs[0])
-                if self.o:  o.save(os.path.join(self.o, fp))
-                else:       o_s.append(o)
-            if not o_s: return None
-            else:       return o_s
+            for fp in get_files(self.i, self.ext, self.recurse)
+                o = args[0](self.i/fp, kwargs[0])
+                o_s.append(fp, o)
+            if not self.o = return o_s
+            for o in o_s: self.save(self.i/fp, o)  # TODO: Change parent dir.
         return fn
 
-class img_dirly:
-    """Dirly `PIL.Image` objs"""
-    def __init__(dirly):
-        pass
+    def save(self, fp, i):
+        return None
 
-class txt_dirly:
-    """Dirly txt files"""
-    def __init__(dirly):
-        pass
+class img_dirly(dirly):
+    "Dirl `PIL.Image` objs"
+    def __init__(self, i, o=None, ext=None, recurse=False):
+        super().__init__(i, o, ext, recurse)
+        if not self.ext: img_extensions
+
+    def save(self, fp, i):
+        i.save(fp)
+
+class txt_dirly(dirly):
+    "Dirl txt files"
+    def __init__(self, i, o=None, recurse=False):
+        super().__init__(i, ext)
+        if not self.ext: ['txt']
+
+    def save(fp, i):
+        pass # TODO
