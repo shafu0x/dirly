8dd63f56020fcb454690b5b7e11a204e060b0ec0
Elfouly Sharif <s.elfouly@enbw.com>
2020-02-02 20:48:01 +0100

    pytest txt_dirly

--- tests/test_txt_dirly.py
+++ tests/test_txt_dirly.py
@@ -1,5 +1,14 @@
 from dirly import txt_dirly
 
-def test_txt_dirly_with_o_dir():pass
+IN_DIR = './tests/texts/data/in'
+OUT_DIR = './tests/texts/data/out'
+
+@txt_dirly(i=IN_DIR, o=OUT_DIR)
+def test_txt_dirly_with_o_dir(_f):
+    with open(_f, 'w') as f:
+        f.write('txt_dirly is very cool!')
+
 
 def test_txt_dirly_without_o_dir(): pass
+
+test_txt_dirly_with_o_dir()
