06fd861afcb3ca94d0697cb670c36eb020bad009
Sharif Elfouly <sharifelfouly@Sharifs-Air.fritz.box>
2020-02-25 20:13:30 +0100

    added desc

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='dirly',
       version='0.1',
       description='Quickly map your function to every file in a directory (verb: to dirl).',
